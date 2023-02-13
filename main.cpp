f27b1a929abf9ef8ac9f8346353f5c54a00f0b14
Yelaman Fazyl <fazil.el2003@outlook.com>
2023-02-13 14:19:13 +0600

    Done litle raycasting with 3D

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <cstdint>
 #include <cassert>
+#include <cmath>
 
 uint32_t pack_color(uint8_t r, uint8_t g, uint8_t b, uint8_t a){
 	return (a << 24) + (b << 16) + (g << 8) + r;
@@ -40,7 +41,7 @@ void draw_rectangle(std::vector<uint32_t>& img, const size_t w, const size_t h, 
 		for(size_t j = 0; j < rect_h; j++){
 			size_t cx = x + i;
 			size_t cy = y + j;
-			assert(cx < w && cy < h);
+			if(cx >= w || cy >= h) continue;
 			
 			img[cx + cy*w] = color;
 		}
@@ -48,64 +49,82 @@ void draw_rectangle(std::vector<uint32_t>& img, const size_t w, const size_t h, 
 }
 
 int main() {
-    int w = 512;
+    int w = 1024;
     int h = 512;
     
     int map_w = 16;
     int map_h = 16;
     
     // map 0-wall, " " - nothing
-    const char map[] =  "0000000000000000"
-			"0000000000000000"
-			"0000    00000000"
-			"0000    00000000"
-			"0000          00"
-			"000    000000000"
-			"000    000000000"
-			"000    000000000"
-			"000    000000000"
-			"000    000000000"
-			"000         0000"
-			"0000000     0000"
-			"000000000   0000"
-			"0000   00   0000"
-			"0000        0000"
-			"0000000000000000";
+    const char map[] = "0000000000000000"\
+                       "0              0"\
+                       "0      00000   0"\
+                       "0     0        0"\
+                       "0     0  0000000"\
+                       "0     0        0"\
+                       "0   00000      0"\
+                       "0   0   0000   0"\
+                       "0   0   0      0"\
+                       "0   0   0  00000"\
+                       "0       0      0"\
+                       "0       0      0"\
+                       "0       0      0"\
+                       "0 0000000      0"\
+                       "0              0"\
+                       "0000000000000000";
 			
     assert(map_w * map_h+1 == sizeof(map));
-    
-    std::vector<uint32_t> framebuffer(w*h, 255);   
-    
-    for(size_t j = 0; j < h; j++){
-    	for(size_t i = 0; i < w; i++){
-    		uint8_t r = 255*j/float(w);
-    		uint8_t g = 0;
-    		uint8_t b = 255*i/float(h);
-    		
-    		framebuffer[i + j*h] = pack_color(r,g,b,255);
-    	}
-    }
-    
-    const size_t rect_w = w / map_w;
+            
+    const size_t rect_w = w /(map_w*2);
     const size_t rect_h = h / map_h;
     
-    double player_x = 8;
-    double player_y = 6;
+    double player_x = 6.5;
+    double player_y = 8.5;
+	double player_a = 1.234;
+	const double fov = M_PI/3;
+
+	for(size_t j = 0; j < 360; j++){
+    	std::vector<uint32_t> framebuffer(w*h, pack_color(255,255,255,255));   
+		
     
-    for(size_t j = 0; j < map_h; j++){
-    	for(size_t i = 0; i < map_w; i++){
-    		if(map[i + j*map_w] != ' ')
-    			continue;
-    			
-    		size_t rect_x = i * rect_w;
-    		size_t rect_y = j * rect_h;
-    		
-    		draw_rectangle(framebuffer, w, h, rect_x, rect_y, rect_w, rect_h, pack_color(255,255,255,255));
-    	}
-    }
+		for(size_t j = 0; j < map_h; j++){
+			for(size_t i = 0; i < map_w; i++){
+				if(map[i + j*map_w] != ' ')
+					continue;
+					
+				size_t rect_x = i * rect_w;
+				size_t rect_y = j * rect_h;
+				
+				draw_rectangle(framebuffer, w, h, rect_x, rect_y, rect_w, rect_h, pack_color(255,0,0,255));
+			}
+		}
+		
+		// player is rectangle with 5x5 size
+		draw_rectangle(framebuffer, w, h, player_x*rect_w, player_y*rect_h, 5, 5, pack_color(255,255,255,255));
+
+		player_a += 2*M_PI/360;
     
-    // player is rectangle with 5x5 size
-    draw_rectangle(framebuffer, w, h, player_x*rect_w, player_y*rect_h, 5, 5, pack_color(255,255,255,255));
+		for(size_t i = 0; i < w/2; i++){
+			double ang = player_a - fov/2 + fov*i/float(w);
+
+			for (float t=0; t<20; t+=.01) {
+				float cx = player_x + t*cos(ang);
+				float cy = player_y + t*sin(ang);
+				if (map[int(cx)+int(cy)*map_w]!=' '){
+					size_t column_height = h/t;
+
+					draw_rectangle(framebuffer, w, h, w/2+i, h/2-column_height/2, 1, column_height, pack_color(255,0,0,255));
+					break;
+				}
+
+				size_t pix_x = cx*rect_w;
+				size_t pix_y = cy*rect_h;
+				framebuffer[pix_x + pix_y*w] = pack_color(160, 160, 160, 255);
+			}
+		}
     
-    draw_ppm("./out.ppm", framebuffer, w, h);
+    	draw_ppm("./out" + std::to_string(j) + ".ppm", framebuffer, w, h);
+	}
+
+	return 0;
 }
